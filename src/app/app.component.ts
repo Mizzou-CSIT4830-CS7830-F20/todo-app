85c6cf58e126da89dacfb284c5b781c3ca5a122e
Professor Wergeles <wergelesn@missouri.edu>
2020-09-23 22:57:22 -0500

    added input, stored to array, ouput array to display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'todo-app';
+
+  todoArray = [];
+
+  addTodo(value) {
+    if (value != '') {
+      this.todoArray.push(value);
+      console.log(this.todoArray);
+    } 
+    else {
+      alert('Todo Field Required!!');
+    }
+  }
 }
